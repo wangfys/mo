fix: fix bugs while computing forward() and backward() in sigmoid refactor: add outSize and inSize attribute for layers to help caculate gradients

layers/Base.py
@@ -1,9 +1,7 @@
 import numpy as np
 import json
-from enum import Enum
-
-forwardStatus = Enum("forwardStatus", ("uninitialized", "initialized", "computed"))
-backwardStatus = Enum("backwardStatus", ("unforwarded", "forwarded", "computed"))
+from functools import reduce
+from ..lib import forwardStatus, backwardStatus
 
 class BaseLayer(object):
     """
@@ -16,12 +14,13 @@ class BaseLayer(object):
             inNode.outNodes.append(self)
         self.outNodes = []
         self.params = []
-        self.inputGradients = []
+        self.inputGradients = {}
         self.paramGradients = []
         self.forwardStatus = forwardStatus.uninitialized
         self.backwardStatus = backwardStatus.unforwarded
         self.inShapes = [inNode.outShape for inNode in self.inNodes]
-    
+        self.inSizes = [np.prod(inNode.outShape) for inNode in self.inNodes]
+
     def init(self, jsonParam=None):
         if jsonParam != None:
             for param in self.params:
@@ -42,6 +41,7 @@ class BaseLayer(object):
                 inNode.forward(feedInput)
             inNode.forwardStatus = forwardStatus.computed
             inNode.backwardStatus = backwardStatus.forwarded
+        print(self.name, self.inShapes)
     
     def preBackward(self):
         for outNode in self.outNodes:

layers/Flatten.py
@@ -10,9 +10,8 @@ class Flatten(BaseLayer):
     """
     def __init__(self, **args):
         BaseLayer.__init__(self, args)
-        self.outShape = np.array((self.inShapes[0][0], 1))
-        for d in self.inShapes[0][1:]:
-            self.outShape[1] *= d
+        self.outShape = np.array((self.inShapes[0][0], np.prod(self.inShapes[0][1:])))
+        self.outSize = np.prod(self.outShape)
     
     def forward(self, feedInput):
         if BaseLayer.forward(self, feedInput):

layers/Sigmoid.py
@@ -8,16 +8,22 @@ class Sigmoid(BaseLayer):
     def __init__(self, **args):
         BaseLayer.__init__(self, args)
         self.outShape = np.array(self.inShapes[0])
+        self.outSize = np.prod(self.outShape)
 
     def forward(self, feedInput):
         if BaseLayer.forward(self, feedInput):
             return None
         inputTensor = np.array(self.inNodes[0].output)
-        self.output = 1 / (1 + np.exp(inputTensor))
+        self.output = 1 / (1 + np.exp(-inputTensor))
     
     def backward(self, applyGradient):
         if BaseLayer.preBackward(self):
             return None
-        inputGradient = np.diag((self.output * (self.output - 1)).flatten())
-        self.inputGradients.append(inputGradient)
+        columnNumber = self.inSizes[0]
+        inputVector = (self.output * (1 - self.output)).flatten()
+        thisGradient = np.diag(inputVector)
+        inputGradient = np.zeros((1, columnNumber))
+        for outNode in self.outNodes:
+            inputGradient = inputGradient + np.dot(outNode.inputGradients[self.name], thisGradient)
+        self.inputGradients[self.inNodes[0].name] = inputGradient
         BaseLayer.backward(self, applyGradient)