refactor: small fix about the shape of gradients

layers/Conv2D.py
@@ -62,9 +62,9 @@ class Conv2D(BaseLayer):
                         thisKGradient[index] = tmpKGradient.flatten()
                         thisBGradient[index] = tmpBGradient.flatten()
                         index += 1
-        inputGradient = np.zeros((1, columnNumber))
-        KGradient = np.zeros((1, self.K.size))
-        bGradient = np.zeros((1, self.b.size))
+        inputGradient = np.zeros([columnNumber])
+        KGradient = np.zeros([self.K.size])
+        bGradient = np.zeros([self.b.size])
         for outNode in self.outNodes:
             inputGradient += np.dot(outNode.inputGradients[self.name], thisInputGradient)
             KGradient += np.dot(outNode.inputGradients[self.name], thisKGradient)

layers/Dense.py
@@ -47,12 +47,12 @@ class Dense(BaseLayer):
             thisBGradient[i*self.outShape[1]:(i+1)*self.outShape[1]] = np.diag(np.ones((self.b.size)))
             for j in range(self.K.shape[0]):
                 thisKGradient[i*self.outShape[1]+j, j*self.K.shape[1]:(j+1)*self.K.shape[1]] = self.inNodes[0].output[j]
-        inputGradient = np.zeros((1, columnNumber))
-        KGradient = np.zeros((1, self.K.size))
-        bGradient = np.zeros((1, self.b.size))
+        inputGradient = np.zeros([columnNumber])
+        KGradient = np.zeros([self.K.size])
+        bGradient = np.zeros([self.b.size])
         for outNode in self.outNodes:
             inputGradient += np.dot(outNode.inputGradients[self.name], thisInputGradient)
-            KGradient +=  np.dot(outNode.inputGradients[self.name], thisKGradient)
+            KGradient += np.dot(outNode.inputGradients[self.name], thisKGradient)
             bGradient += np.dot(outNode.inputGradients[self.name], thisBGradient)
         self.inputGradients[self.inNodes[0].name] = inputGradient
         if not self.fix:

layers/Flatten.py
@@ -23,9 +23,9 @@ class Flatten(BaseLayer):
         if BaseLayer.preBackward(self):
             return None
         columnNumber = self.inSizes[0]
-        inputVector = np.ones((columnNumber))
+        inputVector = np.ones([columnNumber])
         thisInputGradient = np.diag(inputVector)
-        inputGradient = np.zeros((1, columnNumber))
+        inputGradient = np.zeros([columnNumber])
         for outNode in self.outNodes:
             inputGradient += np.dot(outNode.inputGradients[self.name], thisInputGradient)
         self.inputGradients[self.inNodes[0].name] = inputGradient

layers/operators/Add.py
@@ -30,7 +30,7 @@ class Add(BaseLayer):
                 tmp = reduce(np.add, tmp).reshape(self.outShape).flatten()
                 for k in np.argwhere(tmp!=0):
                     thisInputGradient[k, j] = tmp[k]
-            inputGradient = np.zeros((1, rowNumber))
+            inputGradient = np.zeros([rowNumber])
             for outNode in self.outNodes:
                 inputGradient += np.dot(outNode.inputGradients[self.name], thisInputGradient)
             self.inputGradients[self.inNodes[i].name] = inputGradient

layers/operators/LeakyReLU.py
@@ -25,7 +25,7 @@ class LeakyReLU(BaseLayer):
         columnNumber = self.inSizes[0]
         inputVector = np.where(self.output>0, 1, self.k).flatten()
         thisInputGradient = np.diag(inputVector)
-        inputGradient = np.zeros((1, columnNumber))
+        inputGradient = np.zeros([columnNumber])
         for outNode in self.outNodes:
             inputGradient += np.dot(outNode.inputGradients[self.name], thisInputGradient)
         self.inputGradients[self.inNodes[0].name] = inputGradient

layers/operators/Log.py
@@ -25,7 +25,7 @@ class Log(BaseLayer):
         columnNumber = self.inSizes[0]
         inputVector = (1 / np.array(self.inNodes[0].output + self.epsilon)).flatten()
         thisInputGradient = np.diag(inputVector)
-        inputGradient = np.zeros((1, columnNumber))
+        inputGradient = np.zeros([columnNumber])
         for outNode in self.outNodes:
             inputGradient += np.dot(outNode.inputGradients[self.name], thisInputGradient)
         self.inputGradients[self.inNodes[0].name] = inputGradient

layers/operators/Mean.py
@@ -32,7 +32,7 @@ class Mean(BaseLayer):
             tmp = np.mean(tmp, axis=self.axis).flatten()
             for j in np.argwhere(tmp!=0):
                 thisInputGradient[j, i] = tmp[j]
-        inputGradient = np.zeros((1, columnNumber))
+        inputGradient = np.zeros([columnNumber])
         for outNode in self.outNodes:
             inputGradient += np.dot(outNode.inputGradients[self.name], thisInputGradient)
         self.inputGradients[self.inNodes[0].name] = inputGradient

layers/operators/Sigmoid.py
@@ -22,7 +22,7 @@ class Sigmoid(BaseLayer):
         columnNumber = self.inSizes[0]
         inputVector = (self.output * (1 - self.output)).flatten()
         thisInputGradient = np.diag(inputVector)
-        inputGradient = np.zeros((1, columnNumber))
+        inputGradient = np.zeros([columnNumber])
         for outNode in self.outNodes:
             inputGradient += np.dot(outNode.inputGradients[self.name], thisInputGradient)
         self.inputGradients[self.inNodes[0].name] = inputGradient