7ddfdef84a1696aedd356b97796ad49540b210a8
wangfys <fyswang@gmail.com>
2018-12-12 21:12:30 +0800

    refactor: mo.Add can add tensors with different shapes by using reduce(np.add, datalist)

--- layers/operators/Add.py
+++ layers/operators/Add.py
@@ -1,36 +1,37 @@
 import numpy as np
+from functools import reduce
 from ..Base import BaseLayer
+from ...lib import getNumpyShape
 
 class Add(BaseLayer):
     """
-    This is an add operator which can add lots of tensors with same shape.
+    This is an add operator which can add several tensors. It works like reduce(np.add, datalist).
     """
     def __init__(self, **args):
         BaseLayer.__init__(self, args)
-        for i in range(len(self.inShapes)):
-            if (self.inShapes[i] != self.inShapes[0]).all():
-                raise Exception("can not add tensors with different shapes in %s" % self.name)
-        self.outShape = self.inShapes[0]
+        self.outShape = getNumpyShape(reduce(np.add, [np.zeros(inNode.outShape) for inNode in self.inNodes]))
         self.outSize = np.prod(self.outShape)
 
     def forward(self, feedInput):
         if BaseLayer.forward(self, feedInput):
             return None
-        outputTensor = np.zeros(self.outShape)
-        for inNode in self.inNodes:
-            outputTensor += inNode.output
-        self.output = outputTensor
+        self.output = reduce(np.add, [inNode.output for inNode in self.inNodes]).reshape(self.outShape)
     
     def backward(self, applyGradient):
         if BaseLayer.preBackward(self):
             return None
         rowNumber = self.outSize
-        for inNode in self.inNodes:
-            thisInputGradient = np.zeros((rowNumber, rowNumber))
-            for i in range(rowNumber):
-                thisInputGradient[i][i] = 1
+        for i in range(len(self.inNodes)):
+            columnNumber = self.inNodes[i].outSize
+            thisInputGradient = np.zeros((rowNumber, columnNumber))
+            for j in range(columnNumber):
+                tmp = [np.zeros(inNode.outShape) for inNode in self.inNodes]
+                tmp[i].ravel()[j] = 1
+                tmp = reduce(np.add, tmp).reshape(self.outShape).flatten()
+                for k in np.argwhere(tmp!=0):
+                    thisInputGradient[k, j] = tmp[k]
             inputGradient = np.zeros((1, rowNumber))
             for outNode in self.outNodes:
                 inputGradient += np.dot(outNode.inputGradients[self.name], thisInputGradient)
-            self.inputGradients[inNode.name] = inputGradient
+            self.inputGradients[self.inNodes[i].name] = inputGradient
         BaseLayer.backward(self, applyGradient)
