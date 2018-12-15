708d58db49662ee249516b00345d3eb0075a99ed
wangfys <fyswang@gmail.com>
2018-12-15 19:21:39 +0800

    feature: add mo.Constant() operator

---
+++ layer/operator/Constant.py
@@ -0,0 +1,20 @@
+import numpy as np
+from functools import reduce
+from ..Base import BaseLayer
+from ...lib import getNumpyShape
+from ...globalvar import *
+
+class Constant(BaseLayer):
+    """
+    This is the constant layer.
+    """
+    def __init__(self, data):
+        BaseLayer.__init__(self, {"input": []}, inputNum=0)
+        self.input = np.array(data)
+        self.outShape = getNumpyShape(self.input)
+        self.outSize = np.prod(self.outShape)
+        if Config["imperative"]:
+            self.forward({})
+
+    def forward(self, feedInput):
+        self.output = self.input.reshape(self.outShape)
--- layer/operator/__init__.py
+++ layer/operator/__init__.py
@@ -1,4 +1,5 @@
 from .Add import Add
+from .Constant import Constant
 from .Log import Log
 from .Mean import Mean
 from .Sum import Sum
