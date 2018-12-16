fix: edit epsilon from 0 to 1e-10 in case of math overflow

layer/loss/CrossEntropy.py
@@ -6,9 +6,13 @@ from ...globalvar import *
 class CrossEntropy(BaseLayer):
     """
     This is the cross entropy layer.
+    Here are the explanations of args:
+        isTrain: a boolean, False by default
+        epsilon: very small value to be added in case of log(0), 1e-10 by default
     """
     def __init__(self, **args):
         BaseLayer.__init__(self, args, inputNum=2)
+        self.epsilon = args["epsilon"] if "epsilon" in args else 1e-10
         if (self.inShapes[0] != self.inShapes[1]).any():
             raise Exception("the shape of the two inputs of '%s' are not equivalent" % self.name)
         self.outShape = np.array([1])
@@ -18,15 +22,15 @@ class CrossEntropy(BaseLayer):
 
     def calcGradient(self):
         if self.inNodes[0] == self.inNodes[1]:
-            thisInputGradient = -(np.log(self.inNodes[0].output + 1e-10) + 1).reshape((self.outSize, self.inSizes[0]))
+            thisInputGradient = -(np.log(self.inNodes[0].output + self.epsilon) + 1).reshape((self.outSize, self.inSizes[0]))
             inputGradient = reduce(np.add, [np.dot(outNode.inputGradients[self.name], thisInputGradient) for outNode in self.outNodes])
             self.inputGradients[self.inNodes[0].name] = inputGradient
         else:
-            thisInputGradients = [-np.log(self.inNodes[1].output + 1e-10).reshape((self.outSize, self.inSizes[0])), (-self.inNodes[0].output / (self.inNodes[1].output + 1e-10)).reshape((self.outSize, self.inSizes[1]))]
+            thisInputGradients = [-np.log(self.inNodes[1].output + self.epsilon).reshape((self.outSize, self.inSizes[0])), (-self.inNodes[0].output / (self.inNodes[1].output + self.epsilon)).reshape((self.outSize, self.inSizes[1]))]
             inputGradients = []
             for i in range(2):
                 inputGradients.append(reduce(np.add, [np.dot(outNode.inputGradients[self.name], thisInputGradients[i]) for outNode in self.outNodes]))
                 self.inputGradients[self.inNodes[i].name] = inputGradients[i]
 
     def forward(self, feedInput):
-        self.output = np.sum(-self.inNodes[0].output * np.log(self.inNodes[1].output + 1e-10)).reshape(self.outShape)
+        self.output = np.sum(-self.inNodes[0].output * np.log(self.inNodes[1].output + self.epsilon)).reshape(self.outShape)

layer/operator/Log.py
@@ -7,11 +7,11 @@ class Log(BaseLayer):
     """
     This is the log layer.
     Here are the explanations of args:
-        epsilon: very small value to be added in case of log(0)
+        epsilon: very small value to be added in case of log(0), 1e-10 by default
     """
     def __init__(self, **args):
         BaseLayer.__init__(self, args, inputNum=1)
-        self.epsilon = args["epsilon"] if "epsilon" in args else 0
+        self.epsilon = args["epsilon"] if "epsilon" in args else 1e-10
         self.outShape = np.array(self.inShapes[0])
         self.outSize = np.prod(self.outShape)
         if Config["imperative"]: