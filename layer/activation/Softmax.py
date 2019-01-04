fix: fix a bug that may overflow

layer/activation/Softmax.py
@@ -31,7 +31,7 @@ class Softmax(BaseLayer):
 
     def forward(self, feedInput):
         tmpTensor = self.inNodes[0].output
-        tmpTensor = np.exp(tmpTensor)
         for i in range(self.outShape[0]):
+            tmpTensor[i] = np.exp(tmpTensor[i] - np.max(tmpTensor[i]))
             tmpTensor[i] = tmpTensor[i] / np.sum(tmpTensor[i])
         self.output = tmpTensor

layer/loss/CrossEntropy.py
@@ -12,7 +12,7 @@ class CrossEntropy(BaseLayer):
     """
     def __init__(self, **args):
         BaseLayer.__init__(self, args, inputNum=2)
-        self.epsilon = args["epsilon"] if "epsilon" in args else 1e-10
+        self.epsilon = args["epsilon"] if "epsilon" in args else 1e-5
         if (self.inShapes[0] != self.inShapes[1]).any():
             raise Exception("the shape of the two inputs of '%s' are not equivalent" % self.name)
         self.outShape = np.array([1])

layer/operator/Log.py
@@ -11,7 +11,7 @@ class Log(BaseLayer):
     """
     def __init__(self, **args):
         BaseLayer.__init__(self, args, inputNum=1)
-        self.epsilon = args["epsilon"] if "epsilon" in args else 1e-10
+        self.epsilon = args["epsilon"] if "epsilon" in args else 1e-5
         self.outShape = np.array(self.inShapes[0])
         self.outSize = np.prod(self.outShape)
         if Config["imperative"]: