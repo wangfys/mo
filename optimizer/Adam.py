644fa6e9f522b1dd07432a3769f5daff3fb95839
wangfys <fyswang@gmail.com>
2019-01-06 12:59:19 +0800

    feature: add AMSGrad for Adam

--- optimizer/Adam.py
+++ optimizer/Adam.py
@@ -10,6 +10,7 @@ class Adam(BaseOptimizer):
         self.beta1 = args["beta1"] if "beta1" in args else 0.9
         self.beta2 = args["beta2"] if "beta2" in args else 0.999
         self.epsilon = args["epsilon"] if "epsilon" in args else 1e-8
+        self.AMSGrad = args["AMSGrad"] if "AMSGrad" in args else False
         self.m = {}
         self.v = {}
         self.t = 0
@@ -23,9 +24,14 @@ class Adam(BaseOptimizer):
     def applyFunc(self, param, layer, paraName):
         gradient = layer.paramGradients[paraName].flatten()
         self.m[layer.name][paraName] = self.beta1 * self.v[layer.name][paraName] + (1 - self.beta1) * gradient
+        tmpv = self.v[layer.name][paraName]
         self.v[layer.name][paraName] = self.beta2 * self.v[layer.name][paraName] + (1 - self.beta2) * gradient ** 2
         learning_rate = self.learning_rate * np.sqrt(1 - self.beta2 ** self.t) / (1 - self.beta1 ** self.t)
-        return param - learning_rate * self.m[layer.name][paraName] / (np.sqrt(self.v[layer.name][paraName]) + self.epsilon)
+        if self.AMSGrad:
+            tmpv = np.maximum(tmpv, self.v[layer.name][paraName])
+            return param - learning_rate * self.m[layer.name][paraName] / (np.sqrt(tmpv) + self.epsilon)
+        else:
+            return param - learning_rate * self.m[layer.name][paraName] / (np.sqrt(self.v[layer.name][paraName]) + self.epsilon)
 
     def minimize(self, feedInput=None):
         self.t += 1
